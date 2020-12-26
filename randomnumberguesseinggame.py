5f1a8038df085b6cf5287aecb4c9f339cdf010a9
spiritrover <60612506+spiritrover@users.noreply.github.com>
2020-12-26 14:01:42 +0100

    Create randomnumberguesseinggame.py
    v1 of random number guessing game

---
+++ randomnumberguesseinggame.py
@@ -0,0 +1,42 @@
+import random
+
+score = 0
+
+moves = 0
+
+guess = input("please enter your guess here:\n")
+
+def openwrite_close(score):
+    x = str(score)
+    f = open("Scoreforrandomnumberguessinggame", "w")
+    f.write(x)
+    f.close() 
+
+
+while True:
+
+    randomnumber = random.randint(1, 6)   
+
+    if int(guess) == randomnumber:
+
+        score = score+6
+        openwrite_close(score)
+
+    else:
+      
+        score = score-1
+        openwrite_close(score)
+
+    #print(score)        
+
+    if score > 250:
+        print("You Win! Moves:",moves, "last score:", score)
+        exit()
+
+    moves = moves +1
+
+    if moves > 1000:
+        print("You Lose :(, moves:",moves, "last score", score)
+        exit()
+
+
