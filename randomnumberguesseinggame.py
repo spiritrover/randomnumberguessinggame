3c5a4ed09fd075bac902efd10ff080b5e633547a
spiritrover <60612506+spiritrover@users.noreply.github.com>
2020-12-27 18:50:56 +0100

    added credit system

--- randomnumberguesseinggame.py
+++ randomnumberguesseinggame.py
@@ -1,5 +1,5 @@
-
 import random
+credits = 20
 
 def openwrite_close(score):
     x = str(score)
@@ -12,31 +12,41 @@ def game():
     guess = input("please enter your guess here:\n")
     score = 0
     moves = 0
+    
     while True:
 
+        global credits
         randomnumber = random.randint(1, 10)   
 
         if int(guess) == randomnumber:
 
             score = score+10
             openwrite_close(score)
+            
 
         else:
       
             score = score-1
-            openwrite_close(score)      
+            openwrite_close(score)
+                 
 
 
         if score > 250:
             print("You Win! Moves:",moves, "last score:", score)
+            credits = credits + 1
             return()
 
         moves = moves +1
 
         if moves > 1000:
             print("You Lose :(, moves:",moves, "last score", score)
+            credits = credits - 1 
             return()
 
-
 while True:
-    game()
+    if credits >= 1:
+        print("remaining credit:", int(credits))
+        game()
+    else:
+        print("remaining credit:",int(credits)) 
+        exit()
